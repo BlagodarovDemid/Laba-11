2bc06d78c097205fdcd7a1354dd6cd91e826f386
BlagodarovDemid <d.a.Blagodarov@gmail.com>
2023-04-13 09:02:18 +0300

    Создание БД и таблицы для авторизации

--- Program №20 Forms/Form1.cs
+++ Program №20 Forms/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,12 @@ namespace Program__20_Forms
         {
             InitializeComponent();
         }
-
+        SqlConnection cn;
+        private void Registration_Load(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=H:\Website\RegistrationAndLogin\Database.mdf;Integrated Security=True");
+            cn.Open();
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
